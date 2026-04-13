2113374f9878043fe69fffa6a114966f3f26bf5d
unkn0wn-j <wjsrnr470@gmail.com>
2026-04-13 15:32:56 +0900

    - docs     : 문서 수정

--- apps/core/choices.py
+++ apps/core/choices.py
@@ -8,8 +8,8 @@ class Status(models.TextChoices):
 
 
 class TargetType(models.TextChoices):
-    POST = "POST", "게시글"
-    COMMENT = "COMMENT", "댓글"
+    POST = "post", "게시글"
+    COMMENT = "comment", "댓글"
 
 
 class ReportStatus(models.TextChoices):
@@ -25,8 +25,8 @@ class VoteStatus(models.TextChoices):
 
 
 class CommentStatus(models.TextChoices):
-    ACTIVE = "ACTIVE", "활성"
-    DELETED = "DELETED", "삭제"
+    ACTIVE = "active", "활성"
+    DELETED = "deleted", "삭제"
 
 
 class PostStatus(models.TextChoices):
@@ -42,13 +42,13 @@ class UserStatus(models.TextChoices):
 
 
 class ReportReasonType(models.TextChoices):
-    ABUSE = "ABUSE", "욕설/비하/혐오 표현"
-    SPAM = "SPAM", "스팸/광고"
-    FALSE_INFO = "FALSE_INFO", "허위정보"
-    SEXUAL = "SEXUAL", "음란/선정적 컨텐츠"
-    OTHER = "OTHER", "기타"
+    ABUSE = "abuse", "욕설/비하/혐오 표현"
+    SPAM = "spam", "스팸/광고"
+    FALSE_INFO = "false_info", "허위정보"
+    SEXUAL = "sexual", "음란/선정적 컨텐츠"
+    OTHER = "other", "기타"
 
 
 class ReportActionType(models.TextChoices):
-    DELETE = "DELETE", "삭제"
-    KEEP = "KEEP", "유지"
+    DELETE = "delete", "삭제"
+    KEEP = "keep", "유지"
