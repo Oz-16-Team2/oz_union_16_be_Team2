bfec6ea9292b0992b0a9a7d1dc4efa8b9484817c
Brandon-Hyun <theoriginaldongik@gmail.com>
2026-04-14 16:22:52 +0900

    - chore    : 기타

--- apps/users/models.py
+++ apps/users/models.py
@@ -71,3 +71,27 @@ class SocialLogin(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.email} - {self.provider}"
+
+
+class Follow(models.Model):
+    follower_user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="팔로우하는유저id",
+        db_column="follower_id",
+        help_text="팔로우를 하는 유저",
+    )
+    following_user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="팔로우받는유저id",
+        db_column="following_id",
+        help_text="팔로우를 받는 유저",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, help_text="생성일")
+
+    class Meta:
+        db_table = "follows"
+        unique_together = ("follower_user_id", "following_user_id")  # 중복 팔로우 방지
+        verbose_name = "팔로우"
+        verbose_name_plural = "팔로우 목록"
