3c5956796f66a7654c3c8c2bf8b594e5c2868575
Brandon-Hyun <theoriginaldongik@gmail.com>
2026-04-16 16:19:52 +0900

    - feat: 댓글 수정, 삭제 기능 구현

--- apps/posts/views/Comment.py
+++ apps/posts/views/Comment.py
@@ -59,3 +59,57 @@ class PostCommentListCreateView(generics.ListCreateAPIView):  # type: ignore[typ
             raise NotFound("게시글을 찾을 수 없습니다.")
 
         serializer.save(user_id=self.request.user.id, post_id=post_id)
+
+
+@extend_schema_view(
+    patch=extend_schema(
+        summary="REQ-COMM-003: 댓글 수정",
+        description="본인이 작성한 댓글의 내용을 수정합니다.<br>작성자 본인만 호출할 수 있습니다.",
+        tags=["댓글 (Comments)"]
+    ),
+    delete=extend_schema(
+        summary="REQ-COMM-004: 댓글 삭제",
+        description="본인이 작성한 댓글을 삭제합니다. (Soft Delete) <br> 작성자 본인만 호출할 수 있습니다.",
+        tags=["댓글 (Comments)"]
+    )
+)
+class PostCommentDetailView(generics.RetrieveUpdateDestroyAPIView):  # type: ignore[type-arg]
+    """
+    특정 댓글의 수정(PATCH), 삭제(DELETE) 뷰 , 조회 (GET-예비용)
+    URL: /api/v1/posts/{post_id}/comments/{comment_id}
+    """
+    # 수정/삭제는 무조건 로그인한 유저만 가능하므로 IsAuthenticated 적용
+    permission_classes = [IsAuthenticated]
+    
+    lookup_url_kwarg = "comment_id"
+
+    def get_serializer_class(self) -> type[BaseSerializer]:
+        # 수정 시에도 작성의 글자수 제한  재활용
+        return CommentCreateSerializer
+
+    def get_queryset(self) -> QuerySet[Comment]:
+        post_id: Any = self.kwargs.get("post_id")
+        
+        return Comment.objects.filter(post_id=post_id, deleted_at__isnull=True)
+
+    def perform_update(self, serializer: BaseSerializer) -> None:
+        """PATCH: 댓글 수정 로직"""
+        if serializer.instance.user_id != self.request.user.id:
+            raise PermissionDenied("댓글을 수정할 권한이 없습니다.")
+            
+        serializer.save()
+
+    def perform_destroy(self, instance: Comment) -> None:
+        """DELETE: 댓글 삭제 로직 (Soft Delete)"""
+        if instance.user_id != self.request.user.id:
+            raise PermissionDenied("댓글을 삭제할 권한이 없습니다.")
+            
+        #  삭제 시간을 기록 (Soft Delete)
+        instance.deleted_at = timezone.now()
+        instance.save()
+        
+    def destroy(self, request: Any, *args: Any, **kwargs: Any) -> Response:
+        """DELETE 성공 시 응답 포맷"""
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
