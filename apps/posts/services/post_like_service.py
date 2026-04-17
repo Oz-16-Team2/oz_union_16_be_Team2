4970fd974c45b343698b7b98aeae92b1c41481f1
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-04-17 15:44:17 +0900

    - feat     : post likes 기능 추가  - test     : 테스트 코드

--- apps/posts/services/post_like_service.py
+++ apps/posts/services/post_like_service.py
@@ -11,11 +11,11 @@ class PostLikeService:
     def toggle_like(post_id: int, user: Any) -> bool:
 
         post = Post.objects.get(id=post_id)
-        like_queryset = PostLike.objects.filter(post=post, user=user.id)
+        like_queryset = PostLike.objects.filter(post=post, user=user)
 
         if like_queryset.exists():
             like_queryset.delete()
             return False
 
-        PostLike.objects.create(post=post, user=user.id)
+        PostLike.objects.create(post=post, user=user)
         return True
