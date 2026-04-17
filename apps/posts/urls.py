2aa401b4fa7aa543b1b51713492bd90c10a971a5
whdgk7176git config --global user.name whdgk7176 <whdgk7176@gmail.com>
2026-04-17 11:14:41 +0900

    - feat     : post report 기능 추가

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -5,12 +5,14 @@ from apps.posts.views.post_public_views import (
     PostCollectionAPIView,
     PostDetailAPIView,
 )
+from apps.posts.views.post_report_views import PostReportView
 
 app_name = "posts"
 
 urlpatterns = [
     path("", PostCollectionAPIView.as_view(), name="posts-list"),
     path("<int:post_id>/", PostDetailAPIView.as_view(), name="post-detail"),
+    path("<int:post_id>/reports/", PostReportView.as_view(),name="post-report"),
     # <POST>댓글 작성, <GET> 댓글 조회 API
     path("<int:post_id>/comments", PostCommentListCreateView.as_view(), name="post-comments"),
 ]
