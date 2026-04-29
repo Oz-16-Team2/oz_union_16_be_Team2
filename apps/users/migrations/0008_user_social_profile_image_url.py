4f930ba2f53d7737f501cc73a76b500c0360e10f
SuyeongUeno <flrhsaks77@gmail.com>
2026-04-30 00:09:10 +0900

    - fix      : 버그 수정

--- apps/users/migrations/0008_user_social_profile_image_url.py
+++ apps/users/migrations/0008_user_social_profile_image_url.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('users', '0007_alter_user_status'),
+        ("users", "0007_alter_user_status"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='social_profile_image_url',
+            model_name="user",
+            name="social_profile_image_url",
             field=models.URLField(blank=True, null=True),
         ),
     ]
